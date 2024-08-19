44f6ba380ef22fb9b3950265426b7de9251571f1
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-19 14:51:58 +0530

    Bring the Form in the center

--- React_Project/NetFlixGPT/src/components/LoginPage/LoginForm.js
+++ React_Project/NetFlixGPT/src/components/LoginPage/LoginForm.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function LoginForm() {
   return (
-    <form className='absolute'>
-        <div className='flex flex-col'>
+    <form className='absolute flex flex-col items-center justify-center w-screen h-screen'>
+        <div className='flex flex-col w-3/12 py-20 px-10 gap-y-6 rounded-md  bg-slate-900/70 '>
             <h1> Sign In</h1>
             <input type="email" name="email" placeholder="Email or Phone number" />
 
