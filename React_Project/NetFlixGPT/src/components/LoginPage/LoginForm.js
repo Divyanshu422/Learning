bfd254085c7f68bed233843b769b48373dda4ad1
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-19 17:29:52 +0530

    Using the state variable to the Heading -> Login Form vs Sing In Form

--- React_Project/NetFlixGPT/src/components/LoginPage/LoginForm.js
+++ React_Project/NetFlixGPT/src/components/LoginPage/LoginForm.js
@@ -7,14 +7,15 @@ function LoginForm() {
     const [isSignInForm, setIsSignInForm] = useState(true);
     const handlerToggleForm = () => {
         setIsSignInForm(!isSignInForm);
-        console.log(isSignInForm);
     }
   return (
     <form className='absolute flex flex-col items-center justify-center w-screen h-screen'>
         <div className='flex flex-col w-3/12 py-20 px-10 gap-y-6 rounded-md  bg-stone-950/80 '>
 
 
-            <h1 className='text-4xl font-bold text-white w-10/12  mx-auto'> Sign In</h1>
+            <h1 className='text-4xl font-bold text-white w-10/12  mx-auto'>{
+            isSignInForm?  'Sign In': 'Login Form'
+            }</h1>
             <input type="email" name="email" placeholder="Email or Phone number" className='w-10/12 mx-auto rounded-md py-4 px-4 text-neutral-100 bg-slate-600/50' required />
             <input type="password" name="password" placeholder= 'Password' className='w-10/12 mx-auto rounded-md py-4 px-4 text-neutral-100 bg-slate-600/50' required/>
             <button className = 'w-10/12  mx-auto py-2 px-4 text-[#F8F6F4] text-xl font-bold bg-red-700'>SignIn</button>
