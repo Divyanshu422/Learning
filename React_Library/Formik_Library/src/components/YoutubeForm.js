ec0445fd34f9aa0ab837bfd51ce58aac66e0d82b
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-25 01:34:36 +0530

    Passing the Object to the UseFormik hook and linking the input fields with the formik Property initialValue using the onChange and value attribut

--- React_Library/Formik_Library/src/components/YoutubeForm.js
+++ React_Library/Formik_Library/src/components/YoutubeForm.js
@@ -1,26 +1,32 @@
 import React from 'react'
-
-  import { useFormik } from 'formik'
-
+import { useFormik } from 'formik'
 
   function YoutubeForm() {
-    //* The hook consumes the object as parameter and return a object
-    const useFormikObject = useFormik({})
-    console.log('the hook contains multiple properties',useFormikObject);
-    return (
+  
+    const useFormikObject = useFormik({
+      initialValues:{
+        name: '',
+        email: '',
+        channel: ''
+      }
+    })
 
+    console.log(useFormikObject.values);
 
+    return ( 
     <div>
         <form>
-            {/* The value of `htmlFor` attribute shall be same as the `id` attribute of input tag */}
+            {/*  For tracking the fields value and maintaining the state -> we need to use the onChange and value attribute */}
+            {/* The useFormik hook provides the handleChange method, which is used to update the form field values. When a user interacts with an input field (e.g., typing into a text field), handleChange updates the corresponding value in formik.values based on the name attribute of the input field.
+           */}
             <label htmlFor='name'>Name</label>
-            <input type='text' id='name' name='name'  />
+            <input type='text' id='name' name='name' onChange={useFormikObject.handleChange} value={useFormikObject.values.name}/>
 
             <label htmlFor='email'>E-mail</label>
-            <input type='email' id='email' name='email'  />
+            <input type='email' id='email' name='email' onChange={useFormikObject.handleChange} value={useFormikObject.values.email} />
 
             <label htmlFor='channel'>Channel</label>
-            <input type='text' id='channel' name='channel'  />
+            <input type='text' id='channel' name='channel' onChange={useFormikObject.handleChange} value={useFormikObject.values.channel} />
 
             <button> Submit </button>
         </form>
