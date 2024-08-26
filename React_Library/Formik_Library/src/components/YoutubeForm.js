a5913778c69ef44b40f911a85942271db17f97d8
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-26 22:15:33 +0530

    Consoling the touched object

--- React_Library/Formik_Library/src/components/YoutubeForm.js
+++ React_Library/Formik_Library/src/components/YoutubeForm.js
@@ -32,7 +32,7 @@ const validate = ((values)=>{
       onSubmit,
       validate
     })
-    console.log(useFormikObject.errors)
+    console.log(useFormikObject.touched);
     return ( 
     <div>
         <form onSubmit={useFormikObject.handleSubmit}>
@@ -42,23 +42,24 @@ const validate = ((values)=>{
                    id='name' 
                    name='name' 
                    onChange={useFormikObject.handleChange} 
+                   onBlur={useFormikObject.handleBlur}
                    value={useFormikObject.values.name}/>
                    {
-                       useFormikObject.errors.name && <p className='error'>{useFormikObject.errors.name}</p>
+                     useFormikObject.touched.name && useFormikObject.errors.name && <p className='error'>{useFormikObject.errors.name}</p>
                    }
             </div>
             <div className="form-control">
               <label htmlFor='email'>E-mail</label>
-              <input type='email' id='email' name='email' onChange={useFormikObject.handleChange} value={useFormikObject.values.email} />
+              <input type='email' id='email' name='email' onChange={useFormikObject.handleChange} onBlur={useFormikObject.handleBlur} value={useFormikObject.values.email} />
               {
-                useFormikObject.errors.email? <p className='error'>{useFormikObject.errors.email}</p>:null
+                useFormikObject.touched.email && useFormikObject.errors.email? <p className='error'>{useFormikObject.errors.email}</p>:null
               }
             </div>
             <div className="form-control">
               <label htmlFor='channel'>Channel</label>
-              <input type='text' id='channel' name='channel' onChange={useFormikObject.handleChange} value={useFormikObject.values.channel} />
+              <input type='text' id='channel' name='channel' onChange={useFormikObject.handleChange} onBlur={useFormikObject.handleBlur} value={useFormikObject.values.channel} />
               {
-                useFormikObject.errors.channel && <p className='error'>{useFormikObject.errors.channel}</p>  // error message for channel field if it's not provided or empty.  else null  for no error message.
+                 useFormikObject.touched.channel && useFormikObject.errors.channel && <p className='error'>{useFormikObject.errors.channel}</p>  // error message for channel field if it's not provided or empty.  else null  for no error message.
               }
             </div>
 
