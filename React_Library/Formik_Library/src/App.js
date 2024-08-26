fac0ff49b9d1b56b9daa024cd013f7d0c019bc01
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-26 23:11:24 +0530

    Producing the New code by reducing the boilerPlate code in formik

--- React_Library/Formik_Library/src/App.js
+++ React_Library/Formik_Library/src/App.js
@@ -2,10 +2,12 @@
 
       import './App.css'
       import YoutubeForm from './components/YoutubeForm';
+      import NewYoutubeForm from './components/NewYoutubeForm';
       function App() {
         return (
           <div className='App'>
-            <YoutubeForm/>
+            {/* <YoutubeForm/> */}
+            <NewYoutubeForm/>
           </div>
         );
       }
---
+++ React_Library/Formik_Library/src/components/NewYoutubeForm.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import * as yup from 'yup';
+const initialValues ={
+  name: '',
+  email: '',
+  channel: ''
+}
+const onSubmit = (values_Object)=>{
+          
+}
+
+
+// * Validation Schema for yup for all the fields:
+const validationSchema = yup.object({
+  name: yup.string().required('Name is required!'),
+  email: yup.string().email('Invalid email format').required('Email is required'),
+  channel: yup.string().required('Channel name is required')
+})
+  function NewYoutubeForm() {
+    const useFormikObject = useFormik({
+      initialValues,
+      onSubmit,
+      validationSchema
+    })
+    console.log(useFormikObject.touched);
+    return ( 
+    <div>
+        <form onSubmit={useFormikObject.handleSubmit}>
+          <div className="form-control">
+            <label htmlFor='name'>Name</label>
+            <input type='text' 
+                   id='name' 
+                   name='name' 
+                   onChange={useFormikObject.handleChange} 
+                   onBlur={useFormikObject.handleBlur}
+                   value={useFormikObject.values.name}/>
+                   {
+                     useFormikObject.touched.name && useFormikObject.errors.name && <p className='error'>{useFormikObject.errors.name}</p>
+                   }
+            </div>
+            <div className="form-control">
+              <label htmlFor='email'>E-mail</label>
+              <input type='email' id='email' name='email' onChange={useFormikObject.handleChange} onBlur={useFormikObject.handleBlur} value={useFormikObject.values.email} />
+              {
+                useFormikObject.touched.email && useFormikObject.errors.email? <p className='error'>{useFormikObject.errors.email}</p>:null
+              }
+            </div>
+            <div className="form-control">
+              <label htmlFor='channel'>Channel</label>
+              <input type='text' id='channel' name='channel' onChange={useFormikObject.handleChange} onBlur={useFormikObject.handleBlur} value={useFormikObject.values.channel} />
+              {
+                 useFormikObject.touched.channel && useFormikObject.errors.channel && <p className='error'>{useFormikObject.errors.channel}</p>  // error message for channel field if it's not provided or empty.  else null  for no error message.
+              }
+            </div>
+
+            <button type='submit'> Submit </button>
+        </form>
+      
+    </div>
+  )
+}
+
+export default NewYoutubeForm
