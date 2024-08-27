4bb46f6fc584c399e7ae19af2dc76131f881da8c
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-27 13:50:25 +0530

    Field component added to the code

--- React_Library/Formik_Library/src/components/NewYoutubeForm.js
+++ React_Library/Formik_Library/src/components/NewYoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
 const initialValues = {
   name: "",
@@ -26,36 +26,21 @@ function NewYoutubeForm() {
       <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            {...useFormikObject.getFieldProps("name")}
-          />
+          <Field type="text" id="name" name="name" />
           {useFormikObject.touched.name && useFormikObject.errors.name && (
             <p className="error">{useFormikObject.errors.name}</p>
           )}
         </div>
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            {...useFormikObject.getFieldProps("email")}
-          />
+          <Field type="email" id="email" name="email" />
           {useFormikObject.touched.email && useFormikObject.errors.email ? (
             <p className="error">{useFormikObject.errors.email}</p>
           ) : null}
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input
-            type="text"
-            id="channel"
-            name="channel"
-            {...useFormikObject.getFieldProps("channel")}
-          />
+          <Field type="text" id="channel" name="channel" />
           {
             useFormikObject.touched.channel &&
               useFormikObject.errors.channel && (
