97227ec552e7d076693e8f76a971d0e094f6a5ad
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-09-10 12:15:24 +0530

    Running the application using the : npx parcel index.html

--- React_Project/NamasteReact/Lecture2/index.js
+++ React_Project/NamasteReact/Lecture2/index.js
@@ -1,11 +1,10 @@
 const heading = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "This is react code")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello Friends"),
+    React.createElement("h2", {}, "what are u doing"),
+  ])
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
