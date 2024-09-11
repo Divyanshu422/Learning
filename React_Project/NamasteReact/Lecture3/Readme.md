1d294c8a13f6d9b26a262ba27d68df448bf61622
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-09-11 12:09:54 +0530

    Miltiple line jsx code

--- React_Project/NamasteReact/Lecture3/Readme.md
+++ React_Project/NamasteReact/Lecture3/Readme.md
@@ -2,3 +2,13 @@
 
 - Included the script for start and build
 - Writing the jsx inside of the react code and rendering the jsx `See_file: index2.js`
+
+# How jsx is different from the html:
+
+- In JSX, the `className` attribute is used to define a CSS class, whereas in HTML, the `class` attribute is used.
+- In JSX, attribute names follow `camelCase convention` (e.g., onClick), whereas in HTML, attribute names are written in lowercase (e.g., onclick).
+
+# Multiple line jsx code:
+
+- To write JSX across multiple lines, you need to enclose the code inside `parentheses ()` for proper formatting.
+- JSX must be wrapped in a single parent element, such as a `<div> or <React.Fragment>` component. If there are multiple root elements, they should be wrapped inside a parent container or fragment.
--- React_Project/NamasteReact/Lecture3/index2.js
+++ React_Project/NamasteReact/Lecture3/index2.js
@@ -1,10 +1,16 @@
-/*
- * In this index2.js file we will be writing the jsx -> which is nothing but html inside the JavaScript
- */
+// * Multiple line jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// * Coding the react code using the jsx
-const jsxElement = <h1>Namaste react using JSX</h1>;
+/*
+ * To write JSX across multiple lines, you need to enclose the code inside parentheses () for proper formatting.
+ * JSX must be wrapped in a single parent element, such as a <div> or <React.Fragment> component. If there are multiple root elements, they should be wrapped inside a parent container or fragment.
+ */
+const jsxElement = (
+  <div>
+    <h1>Namaste react using JSX</h1>
+    <h2>Thansks for visiting the react code</h2>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxElement);
