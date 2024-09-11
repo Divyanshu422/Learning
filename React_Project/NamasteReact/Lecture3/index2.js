1a704aad605c6a76b1e7d15a5710474b4a38d38a
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-09-11 12:58:23 +0530

    jsx Element adding in the component using the curly bracket

--- React_Project/NamasteReact/Lecture3/index2.js
+++ React_Project/NamasteReact/Lecture3/index2.js
@@ -3,11 +3,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const data = 100;
+
+//* jsx Element adding in the component using the curly bracket
+const jsxElement = (
+  <h1>
+    {" "}
+    React element which will be converted into the react.createElement by babel
+  </h1>
+);
 const TitleComponent = () => (
   <div>
     <h1>this is the title component</h1>
     {/* Injecting the js inside the jsx -> using the curly brackets */}
     <h2>{data}</h2>
+    <p>{jsxElement}</p>
     {console.log("js code inside the jsx")}
   </div>
 );
