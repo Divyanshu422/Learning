3254f67193ae6a0a5d7b67b7aa136f7609203e75
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-09-17 14:58:43 +0530

    Using the Joins method for the Cuisine + Cloudinary ImageId

--- React_Project/NamasteReact/Lecture4/App.js
+++ React_Project/NamasteReact/Lecture4/App.js
@@ -2,6 +2,48 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import logo from "./Assets/FoodLog.png";
 
+export const resData = {
+  id: "655665",
+  name: "Subway",
+  cloudinaryImageId:
+    "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/17/c0e795b0-8a23-4a11-bd90-0b08e4ecca9e_655665.jpg",
+  locality: "Jasola Apollo Metro Station",
+  areaName: "Pacific Jasola",
+  costForTwo: "₹350 for two",
+  cuisines: ["Salads", "Snacks", "Desserts", "Beverages"],
+  avgRating: 4.2,
+  parentId: "2",
+  avgRatingString: "4.2",
+  totalRatingsString: "430",
+  sla: {
+    deliveryTime: 25,
+    lastMileTravel: 2.3,
+    serviceability: "SERVICEABLE",
+    slaString: "25-30 mins",
+    lastMileTravelString: "2.3 km",
+    iconType: "ICON_TYPE_EMPTY",
+  },
+  availability: {
+    nextCloseTime: "2024-09-18 01:00:00",
+    opened: true,
+  },
+  badges: {
+    imageBadges: [
+      {
+        imageId: "Green%20Dot%20Awards/Best%20In%20Veg%20Salad.png",
+        description: "Delivery!",
+      },
+    ],
+    textExtendedBadges: [
+      {
+        iconId: "guiltfree/GF_Logo_android_3x",
+        shortDescription: "options available",
+        fontColor: "#7E808C",
+      },
+    ],
+  },
+};
+
 export const Header = () => {
   return (
     <div className="header-container">
@@ -24,20 +66,24 @@ export const Header = () => {
 const styleCard = {
   backgroundColor: "#FFE5CF",
 };
-const RestaurantCard = ({ resName, cusine, rating }) => {
-  console.log(resName, cusine, rating);
+const RestaurantCard = ({ resData }) => {
+  console.log("the resData is ", resData?.cuisines);
+
   return (
     <div className="restaurant-card" style={styleCard}>
       {/* Using 2 brackets for providing the styles -> outer bracket represent the jsx and inner bracket -> Css_object */}
       <img
-        src="https://b.zmtcdn.com/data/reviews_photos/96d/7c3548c12a019085cb6f8cca8e79496d_1641470996.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData?.cloudinaryImageId
+        }
         alt="res-logo"
         className="image-card"
       />
-      <h3 style={{ color: "#557C56" }}>{resName}</h3>
-      <h4> {cusine}</h4>
-      <h4> {rating}</h4>
-      <h4> 38 min </h4>
+      <h3 style={{ color: "#557C56" }}>{resData?.name}</h3>
+      <h4> {resData?.cuisines.join(", ")}</h4>
+      <h4> {resData.avgRating} stars</h4>
+      <h4> {resData.costForTwo} </h4>
     </div>
   );
 };
@@ -49,16 +95,7 @@ const Body = () => {
         <h3>Search Content</h3>
       </div>
       <div className="restaurant-container">
-        <RestaurantCard
-          resName="Dhaba Estd 1986 Delhi"
-          cusine="North Indian, Desserts"
-          rating={4.4}
-        />
-        <RestaurantCard
-          resName={"Burger King"}
-          cusine="Fast Food, Burgers"
-          rating="4.2"
-        />
+        <RestaurantCard resData={resData} />
       </div>
     </div>
   );
