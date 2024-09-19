e0408e16d941488e56fccfca56b2472ade0d7c82
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-09-19 06:00:35 +0530

    Applying the filter function to the button

--- React_Project/NamasteReact/Lecture4/Readme.md
+++ React_Project/NamasteReact/Lecture4/Readme.md
@@ -29,3 +29,9 @@
   - utils
   - constant/config etc and place the hardcoded data in this folder
   - The name shall start with small letters
+
+### At 1hour 15 min -> we are creating a button on which when we click only the top rated restaurant is shown.
+
+- By top rated restaurant, we means rating is greater than 4.
+- Create the button and use the onClick method which calls the callback function [becoz we are passing the data parameter]
+- create the state variable and onClick update the state variable -> on this state variable call the map function
--- React_Project/NamasteReact/Lecture4/src/components/Body.jsx
+++ React_Project/NamasteReact/Lecture4/src/components/Body.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import {data} from '../utils/mockData'
 import RestaurantCard from './RestaurantCard'
+import { useState } from'react';
 function Body() {
+    console.log(data[0].info);
+  // Creating the state_Variable whose value changes with the onClick of button
+  const [filteredData, setFilteredData] = useState(data);
   return (
     <div>
         <div className="body">
-            <div>
-                <h3>Search Content</h3>
+            <div className='fliter'>
+                <button className = 'filter-btn' onClick={()=>{
+                    // Filtering the data based on cost for two
+                    setFilteredData(data.filter(item => item.info.avgRating >= 4.8))
+                }}>Top rated button</button>
+                
             </div>
             <div className="restaurant-container">
-                {data.map((data, index) => (
+                {filteredData.map((data, index) => (
                 <RestaurantCard key={data?.info?.id} resData={data.info} />
                 ))}
             </div>
